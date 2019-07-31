9bdb53d61a5ce07722d3f7718f4632c8cf4ac3b6
Jonzze <Balenor123@gmail.com>
2019-07-31 16:44:03 +0300

    Add .gitignore

--- code/build.bat
+++ code/build.bat
@@ -2,8 +2,5 @@
 
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
-cl -MT -nologo -Gm- -GR- -EHa- -Ox -Oi -Zi -FC -W3 -wd4201 -wd4100 -wd4189 ../code/main.c /link -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib /out:dev.exe
+cl -MT -nologo -Gm- -GR- -EHa- -Ox -Oi -Zi -FC -W3 -wd4201 -wd4100 -wd4189 ../code/main.c /link -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib /out:fire.exe
 popd
-
-
-
